13e6db65ef87bb840288c5d4c5f53fe711df117d
Mikhail Sergeev <mikhail.s@miro.com>
2020-12-03 20:22:19 +0500

    Added sync predicate to DataStore button, added two more buttons showcasing graphql API

--- src/containers/participants-count-stage/index.tsx
+++ src/containers/participants-count-stage/index.tsx
@@ -5,28 +5,52 @@ import classNames from 'classnames'
 import 'mirotone/dist/styles.css'
 import './styles.less'
 import {Person} from "../../models";
-import {DataStore} from "@aws-amplify/datastore";
+import {API, graphqlOperation, Amplify} from "aws-amplify";
+import * as queries from "../../graphql/queries";
+import * as mutations from "../../graphql/mutations";
 
 export class ParticipantsCountStage extends React.Component {
-	render() {
-		return (
-			<SidebarLayout>
-				<h1 className="h2">Choose retro format</h1>
-				<button className={classNames('button', 'button-primary')} onClick={this.handleCreateClick}>Create</button>
-				<button className={classNames('button', 'button-primary')} onClick={this.handleReadClick}>Read</button>
-			</SidebarLayout>
-		)
-	}
+    render() {
+        return (
+            <SidebarLayout>
+                <h1 className="h2">Choose Retro Format</h1>
+                <button className={classNames('button', 'button-primary')} onClick={this.handleCreateClick}>Create
+                </button>
+                <button className={classNames('button', 'button-primary')} onClick={this.handleReadClick}>Read</button>
+                <button className={classNames('button', 'button-secondary')}
+                        onClick={this.handleCreateClickGraphQL}>Create Graph
+                </button>
+                <button className={classNames('button', 'button-secondary')} onClick={this.handleReadClickGraphQL}>Read
+                    Graph
+                </button>
+            </SidebarLayout>
+        )
+    }
 
-	async handleCreateClick() {
-		await DataStore.save(new Person({
-			miroUserId: 123
-		}))
-	}
+    async handleCreateClick() {
+        const person = await Amplify.DataStore.save(new Person({
+            miroUserId: 985678
+        }), (person) => person.miroUserId('ne', 0))
+        console.log('created person: ', person)
+    }
 
-	async handleReadClick() {
-		let persons = await DataStore.query(Person)
-		console.log(persons)
-	}
+    async handleReadClick() {
+        let persons = await Amplify.DataStore.query(Person)
+        console.log('retrieved persons: ', persons)
+    }
+
+    async handleCreateClickGraphQL() {
+        const person = await API.graphql(graphqlOperation(mutations.createPerson, {
+                input: new Person({
+                        miroUserId: 1234321
+                    })
+        }))
+        console.log('created person: ', person)
+    }
+
+    async handleReadClickGraphQL() {
+        let persons = await API.graphql(graphqlOperation(queries.listPersons))
+        console.log('retrieved persons: ', persons)
+    }
 }
 
--- src/sidebar.tsx
+++ src/sidebar.tsx
@@ -3,8 +3,12 @@ import * as ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './store/store'
 import PagesContainer from './containers/pages-container'
+import {Amplify} from "aws-amplify";
+import awsExports from "./aws-exports";
 
 miro.onReady(() => {
+    Amplify.configure(awsExports);
+
     ReactDOM.render(
         <Provider store={store}>
             <PagesContainer />
