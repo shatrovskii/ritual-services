58767c77af8638a10ea0e797a24a58c05c222904
Ilya Shlykov <ilia.s@miro.com>
2020-12-04 11:11:44 +0500

    firebase

---
+++ src/firebaseconfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyAEb357vQaaKpuy0R7h1rkn8Dow_9YpBTQ",
+	authDomain: "retrobuilder-2c0e4.firebaseapp.com",
+	projectId: "retrobuilder-2c0e4",
+	storageBucket: "retrobuilder-2c0e4.appspot.com",
+	messagingSenderId: "523747200127",
+	appId: "1:523747200127:web:a4a5605ec55816689b834a"
+}
